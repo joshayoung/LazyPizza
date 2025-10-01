036428bd435a1e73a2edfc94d732dc39c3118f52
Josh Young <18503267+joshayoung@users.noreply.github.com>
2025-10-01 18:16:29 -0500

    Add font sizes

--- app/src/main/java/com/joshayoung/lazypizza/ui/theme/Type.kt
+++ app/src/main/java/com/joshayoung/lazypizza/ui/theme/Type.kt
@@ -2,33 +2,62 @@ package com.joshayoung.lazypizza.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.joshayoung.lazypizza.R
 
-// Set of Material typography styles to start with
+val instrumentSansMedium = FontFamily(Font(R.font.instrument_sans_medium))
+val instrumentSansSemiBold = FontFamily(Font(R.font.instrument_sans_semibold))
+val instrumentSansRegular = FontFamily(Font(R.font.instrumentsans_regular))
+
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = instrumentSansSemiBold,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp,
+        lineHeight = 28.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = instrumentSansSemiBold,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 24.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = instrumentSansSemiBold,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 15.sp,
+        lineHeight = 22.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = instrumentSansSemiBold,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = instrumentSansRegular,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        lineHeight = 22.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyMedium = TextStyle(
+        fontFamily = instrumentSansMedium,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = instrumentSansRegular,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 18.sp,
+    ),
+
+    // body-3-medium ?
+
+    // body-4-regular ?
+
 )
