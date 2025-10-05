11793ae4fd11486f75a7702b94a62e5e52932224
Josh Young <18503267+joshayoung@users.noreply.github.com>
2025-10-05 18:46:08 -0500

    Improve search field

--- app/src/main/java/com/joshayoung/lazypizza/search/presentation/searchItems/SearchItemsScreen.kt
+++ app/src/main/java/com/joshayoung/lazypizza/search/presentation/searchItems/SearchItemsScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -14,15 +15,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -37,6 +42,8 @@ import com.joshayoung.lazypizza.core.presentation.components.LazyPizzaScaffold
 import com.joshayoung.lazypizza.search.ImageResource
 import com.joshayoung.lazypizza.search.presentation.components.LazyImage
 import com.joshayoung.lazypizza.ui.theme.LazyPizzaTheme
+import com.joshayoung.lazypizza.ui.theme.SearchIcon
+import kotlinx.coroutines.delay
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -70,7 +77,29 @@ fun SearchItemsScreen(state: SearchItemsState) {
                 state =
                     TextFieldState(
                         initialText = "Search for delicious food…"
-                    )
+                    ),
+                decorator = {
+                    Row() {
+                        Icon(
+                            imageVector = SearchIcon,
+                            contentDescription = null,
+                            tint = MaterialTheme.colorScheme.error
+                        )
+
+                        Box(modifier = Modifier
+                            .padding(start = 10.dp)) {
+                            Text("Search for delicious food…")
+
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+                    .border(1.dp, MaterialTheme.colorScheme.surfaceVariant, shape = RoundedCornerShape(20.dp))
+                    .shadow(elevation = 1.dp, shape = RoundedCornerShape(20.dp))
+                    .background(MaterialTheme.colorScheme.surfaceVariant)
+                    .padding(10.dp)
+                    .fillMaxWidth()
             )
             Column(
                 modifier =
