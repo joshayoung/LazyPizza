868da65d3d03942197b1cb8ecdb3ca2559b60609
Josh Young <18503267+joshayoung@users.noreply.github.com>
2025-11-05 17:21:01 -0600

    Add Text Fields to Login Page with Focus Restrictions (only partially working)

--- app/src/main/java/com/joshayoung/lazypizza/auth/presentation/LoginScreen.kt
+++ app/src/main/java/com/joshayoung/lazypizza/auth/presentation/LoginScreen.kt
@@ -1,7 +1,9 @@
 package com.joshayoung.lazypizza.auth.presentation
 
+import android.R.attr.text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -31,12 +34,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.joshayoung.lazypizza.core.presentation.utils.previewOrders
 import com.joshayoung.lazypizza.core.ui.theme.LazyPizzaTheme
 import com.joshayoung.lazypizza.core.ui.theme.surfaceHighest
 import org.koin.androidx.compose.koinViewModel
@@ -69,12 +76,6 @@ fun LoginScreen(
 ) {
     var sent by remember { mutableStateOf(true) }
     var number by remember { mutableStateOf("") }
-    val code1 = remember { mutableStateOf("") }
-    val code2 = remember { mutableStateOf("") }
-    val code3 = remember { mutableStateOf("") }
-    val code4 = remember { mutableStateOf("") }
-    val code5 = remember { mutableStateOf("") }
-    val code6 = remember { mutableStateOf("") }
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -125,12 +126,19 @@ fun LoginScreen(
         )
 
         if (sent) {
-            val nextFocus = remember { FocusRequester() }
-            val nextFocus1 = remember { FocusRequester() }
-            val nextFocus2 = remember { FocusRequester() }
-            val nextFocus3 = remember { FocusRequester() }
-            val nextFocus4 = remember { FocusRequester() }
-            val nextFocus5 = remember { FocusRequester() }
+            var code1 by remember { mutableStateOf("") }
+            var code2 by remember { mutableStateOf("") }
+            var code3 by remember { mutableStateOf("") }
+            var code4 by remember { mutableStateOf("") }
+            var code5 by remember { mutableStateOf("") }
+            var code6 by remember { mutableStateOf("") }
+
+            val focus1 = remember { FocusRequester() }
+            val focus2 = remember { FocusRequester() }
+            val focus3 = remember { FocusRequester() }
+            val focus4 = remember { FocusRequester() }
+            val focus5 = remember { FocusRequester() }
+            val focus6 = remember { FocusRequester() }
 
             Row(
                 modifier =
@@ -138,19 +146,186 @@ fun LoginScreen(
                         .padding(top = 14.dp)
                         .fillMaxWidth()
             ) {
-                SmsField(code1, nextFocus, nextFocus1, modifier = Modifier.weight(1f))
-                SmsField(code2, nextFocus1, nextFocus2, modifier = Modifier.weight(1f))
-                SmsField(code3, nextFocus2, nextFocus3, modifier = Modifier.weight(1f))
-                SmsField(code4, nextFocus3, nextFocus4, modifier = Modifier.weight(1f))
-                SmsField(code5, nextFocus4, nextFocus5, modifier = Modifier.weight(1f))
-                SmsField(code6, nextFocus5, nextFocus, modifier = Modifier.weight(1f))
+                OutlinedTextField(
+                    value = code1,
+                    onValueChange = {
+                        code1 = it
+                        if (it.isNotEmpty()) {
+                            focus2.requestFocus()
+                        }
+                    },
+                    keyboardOptions =
+                        KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Next
+                        ),
+                    modifier =
+                        Modifier
+                            .weight(1f)
+                            .focusRequester(focus1)
+                )
+                OutlinedTextField(
+                    value = code2,
+                    onValueChange = {
+                        code2 = it
+                        if (it.isNotEmpty()) {
+                            focus3.requestFocus()
+                        }
+                    },
+                    keyboardOptions =
+                        KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Next
+                        ),
+                    modifier =
+                        Modifier
+                            .weight(1f)
+                            .focusRequester(focus2)
+                            .onFocusChanged { focusState ->
+                                if (focusState.isFocused && code1.isEmpty()) {
+                                    focus1.requestFocus()
+                                }
+                            }
+                )
+                OutlinedTextField(
+                    value = code3,
+                    onValueChange = {
+                        code3 = it
+                        if (it.isNotEmpty()) {
+                            focus4.requestFocus()
+                        }
+                    },
+                    keyboardOptions =
+                        KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Next
+                        ),
+                    modifier =
+                        Modifier
+                            .weight(1f)
+                            .focusRequester(focus3)
+                            .onFocusChanged { focusState ->
+                                if (focusState.isFocused && code2.isEmpty()) {
+                                    focus2.requestFocus()
+                                }
+                            }
+                )
+                OutlinedTextField(
+                    value = code4,
+                    onValueChange = {
+                        code4 = it
+                        if (it.isNotEmpty()) {
+                            focus5.requestFocus()
+                        }
+                    },
+                    keyboardOptions =
+                        KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Next
+                        ),
+                    modifier =
+                        Modifier
+                            .weight(1f)
+                            .focusRequester(focus4)
+                            .onFocusChanged { focusState ->
+                                if (focusState.isFocused && code3.isEmpty()) {
+                                    focus3.requestFocus()
+                                }
+                            }
+                )
+                OutlinedTextField(
+                    value = code5,
+                    onValueChange = {
+                        code5 = it
+                        if (it.isNotEmpty()) {
+                            focus6.requestFocus()
+                        }
+                    },
+                    keyboardOptions =
+                        KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Next
+                        ),
+                    modifier =
+                        Modifier
+                            .weight(1f)
+                            .focusRequester(focus5)
+                            .onFocusChanged { focusState ->
+                                if (focusState.isFocused && code4.isEmpty()) {
+                                    focus4.requestFocus()
+                                }
+                            }
+                )
+                OutlinedTextField(
+                    value = code6,
+                    onValueChange = {
+                        if (code6.isEmpty()) {
+                            code6 = it
+                        }
+                    },
+                    keyboardOptions =
+                        KeyboardOptions.Default.copy(
+                            imeAction = ImeAction.Next
+                        ),
+                    modifier =
+                        Modifier
+                            .weight(1f)
+                            .focusRequester(focus6)
+                            .onFocusChanged { focusState ->
+                                if (focusState.isFocused && code5.isEmpty()) {
+                                    focus5.requestFocus()
+                                }
+                            }
+                )
+//                SmsField(
+//                    null,
+//                    null,
+//                    code1,
+//                    nextFocus,
+//                    nextFocus1,
+//                    modifier = Modifier.weight(1f)
+//                )
+//                SmsField(
+//                    code1,
+//                    nextFocus,
+//                    code2,
+//                    nextFocus1,
+//                    nextFocus2,
+//                    modifier = Modifier.weight(1f)
+//                )
+//                SmsField(
+//                    code2,
+//                    nextFocus1,
+//                    code3,
+//                    nextFocus2,
+//                    nextFocus3,
+//                    modifier = Modifier.weight(1f)
+//                )
+//                SmsField(
+//                    code3,
+//                    nextFocus2,
+//                    code4,
+//                    nextFocus3,
+//                    nextFocus4,
+//                    modifier = Modifier.weight(1f)
+//                )
+//                SmsField(
+//                    code4,
+//                    nextFocus3,
+//                    code5,
+//                    nextFocus4,
+//                    nextFocus5,
+//                    modifier = Modifier.weight(1f)
+//                )
+//                SmsField(
+//                    code5,
+//                    nextFocus4,
+//                    code6,
+//                    nextFocus5,
+//                    nextFocus,
+//                    modifier = Modifier.weight(1f)
+//                )
             }
 
             Button(
                 onClick = {
                     verifyCode(
-                        code1.value + code2.value + code3.value +
-                            code4.value + code5.value + code6.value
+                        "${code1}${code2}${code3}${code4}${code5}$code6"
                     )
                     sent = true
                 },
@@ -191,6 +366,8 @@ fun LoginScreen(
 
 @Composable
 fun SmsField(
+    previousCode: MutableState<String>?,
+    previousFocusRequester: FocusRequester?,
     code: MutableState<String>,
     focusRequester: FocusRequester,
     focusRequesterNext: FocusRequester,
@@ -236,16 +413,27 @@ fun SmsField(
                 .background(MaterialTheme.colorScheme.surfaceHighest)
                 .onFocusChanged { focused ->
                     isFocused = focused.isFocused
-                }.focusOrder(focusRequester) {
-                    next = focusRequesterNext
-                }.onKeyEvent { event ->
-                    if (event.key == Key.Tab) {
-                        focusRequesterNext.requestFocus()
-                        true
-                    } else {
-                        false
+                    if (previousCode != null && previousFocusRequester != null) {
+                        if (previousCode.value.isEmpty()) {
+                            focusRequester.freeFocus()
+                            previousFocusRequester.requestFocus()
+                        }
                     }
+
+//                    if (focused.isFocused && code.value.isEmpty()) {
+//                        focusRequester.requestFocus()
+//                    }
                 }
+//                .focusOrder(focusRequester) {
+//                    next = focusRequesterNext
+//                }.onKeyEvent { event ->
+//                    if (event.key == Key.Tab) {
+//                        focusRequesterNext.requestFocus()
+//                        true
+//                    } else {
+//                        false
+//                    }
+//                }
     )
 }
 
