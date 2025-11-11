9369d557fe7d0bda9a21e5f3470407ee35e244e1
Josh Young <18503267+joshayoung@users.noreply.github.com>
2025-11-11 17:41:14 -0600

    Correct preview for tablet using system UI

--- app/src/main/java/com/joshayoung/lazypizza/menu/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/joshayoung/lazypizza/menu/presentation/home/HomeScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -393,8 +394,9 @@ fun SearchItemsScreenPreview() {
 @Preview(showBackground = true, showSystemUi = true)
 @Preview(
     showBackground = true,
-    widthDp = 800,
-    heightDp = 1280
+    showSystemUi = true,
+//    device = Devices.PIXEL_TABLET
+    device = "spec:width=800dp,height=1280dp,dpi=480"
 )
 private fun HomeScreenPreview() {
     LazyPizzaTheme {
