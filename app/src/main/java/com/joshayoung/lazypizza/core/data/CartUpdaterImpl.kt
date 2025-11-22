740376314b1f89f5ecf130100d9fdfa9976513c4
Josh Young <18503267+joshayoung@users.noreply.github.com>
2025-11-21 18:51:26 -0600

    Move duplicated flow to CartUpdater

--- app/src/main/java/com/joshayoung/lazypizza/core/data/CartUpdaterImpl.kt
+++ app/src/main/java/com/joshayoung/lazypizza/core/data/CartUpdaterImpl.kt
@@ -4,7 +4,17 @@ import com.joshayoung.lazypizza.core.data.network.dto.ToppingInCartDto
 import com.joshayoung.lazypizza.core.domain.CartRepository
 import com.joshayoung.lazypizza.core.domain.CartUpdater
 import com.joshayoung.lazypizza.core.domain.models.Product
+import com.joshayoung.lazypizza.core.presentation.models.InCartItemSingleUi
+import com.joshayoung.lazypizza.core.presentation.models.InCartItemUi
+import com.joshayoung.lazypizza.core.presentation.utils.getMenuTypeEnum
+import com.joshayoung.lazypizza.menu.data.mappers.toInCartItemUi
 import com.joshayoung.lazypizza.menu.presentation.models.ToppingUi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flowOn
+import kotlin.collections.component1
+import kotlin.collections.component2
 
 // TODO: Could both 'insertProductWithToppings' be combined together?
 class CartUpdaterImpl(
@@ -49,5 +59,78 @@ class CartUpdaterImpl(
                 }
             }
         }
+    }
+
+    override suspend fun productsInCart(): Flow<List<InCartItemUi>> {
+        var count = 0
+        return cartRepository
+            .productsInCartWithNoToppings()
+            .combine(
+                cartRepository
+                    .productsInCartWithToppings()
+            ) { productWithNoToppings, productWithToppings ->
+                val groupedByProductId = productWithNoToppings.groupBy { it.productId }
+                val inCartItemUis =
+                    groupedByProductId.map { (_, productList) ->
+                        productList.toInCartItemUi(key = ++count)
+                    }
+
+                val withToppings =
+                    productWithToppings.map { iic ->
+                        val toppings =
+                            cartRepository.getToppingForProductInCart(
+                                iic.lineItemId
+                            )
+                        val toppingsForDisplay =
+                            toppings
+                                .groupBy { it.name }
+                                .mapValues { entry -> entry.value.size }
+                        // could this be generic object?
+                        InCartItemSingleUi(
+                            name = iic.name,
+                            description = iic.description,
+                            imageResource = iic.imageResource,
+                            toppingsForDisplay = toppingsForDisplay,
+                            toppings = toppings,
+                            lineItemId = iic.lineItemId,
+                            imageUrl = iic.imageUrl,
+                            nameWithToppingIds = iic.nameWithToppingIds,
+                            type = getMenuTypeEnum(iic.type),
+                            price = iic.price,
+                            remoteId = iic.remoteId,
+                            productId = iic.productId
+                        )
+                    }
+
+                // group by something more unique:
+                val grp = withToppings.groupBy { it.nameWithToppingIds }
+
+                val inCartItemsWithToppingUis =
+                    grp.map { item ->
+                        val lineNumbers = item.value.map { it.lineItemId }
+                        InCartItemUi(
+                            key = ++count,
+                            lineNumbers = lineNumbers,
+                            name = item.value.first().name,
+                            description = item.value.first().description,
+                            imageResource = item.value.first().imageResource,
+                            toppingsForDisplay = item.value.first().toppingsForDisplay,
+                            toppings = item.value.first().toppings,
+                            imageUrl = item.value.first().imageUrl,
+                            type =
+                                getMenuTypeEnum(
+                                    item.value
+                                        .first()
+                                        .type.name
+                                ),
+                            price = item.value.first().price,
+                            remoteId = item.value.first().remoteId,
+                            productId = item.value.first().productId,
+                            numberInCart = lineNumbers.count()
+                        )
+                    }
+
+                inCartItemUis + inCartItemsWithToppingUis
+            }.flowOn(Dispatchers.Default)
     }
 }
--- app/src/main/java/com/joshayoung/lazypizza/core/domain/CartUpdater.kt
+++ app/src/main/java/com/joshayoung/lazypizza/core/domain/CartUpdater.kt
@@ -2,7 +2,9 @@ package com.joshayoung.lazypizza.core.domain
 
 import com.joshayoung.lazypizza.core.data.network.dto.ToppingInCartDto
 import com.joshayoung.lazypizza.core.domain.models.Product
+import com.joshayoung.lazypizza.core.presentation.models.InCartItemUi
 import com.joshayoung.lazypizza.menu.presentation.models.ToppingUi
+import kotlinx.coroutines.flow.Flow
 
 interface CartUpdater {
     suspend fun insertProductWithToppings(
@@ -15,4 +17,6 @@ interface CartUpdater {
         product: Product?,
         toppingsUi: List<ToppingUi>
     )
+
+    suspend fun productsInCart(): Flow<List<InCartItemUi>>
 }
